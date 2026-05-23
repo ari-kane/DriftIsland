7549ae94bb3087c9fc0f643282a1c59a3c39ec52
Ari Kane <ari_kane@icloud.com>
2026-05-22 20:38:26 -0600

    Initial Commit

---
+++ app2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app2
+//
+//  Created by Work on 5/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app2/app2App.swift
@@ -0,0 +1,17 @@
+//
+//  app2App.swift
+//  app2
+//
+//  Created by Work on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct app2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
